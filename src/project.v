7f422b5af9ee6241f923a75330ae54759adb950b
bewratcha <105075915+bewratcha@users.noreply.github.com>
2026-09-24 13:51:35 -0400

    Refactor module and add copyright notice

--- src/project.v
+++ src/project.v
@@ -1,16 +1,29 @@
-// Change the module name!
-module tt_um_uwasic_onboarding_siratcha_boonsakdee(...) 
-  // Add this inside the module block
-  assign uio_oe = 8'hFF; // Set all IOs to output
-  
-  // Create wires to refer to the values of the registers
+/*
+ * Copyright (c) 2024 Siratcha Boonsakdee
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+`default_nettype none
+
+module tt_um_uwasic_onboarding_siratcha_boonsakdee (
+    input  wire [7:0] ui_in,
+    output wire [7:0] uo_out,
+    input  wire [7:0] uio_in,
+    output wire [7:0] uio_out,
+    output wire [7:0] uio_oe,
+    input  wire       ena,
+    input  wire       clk,
+    input  wire       rst_n
+);
+
+  assign uio_oe = 8'hFF;
+
   wire [7:0] en_reg_out_7_0;
   wire [7:0] en_reg_out_15_8;
   wire [7:0] en_reg_pwm_7_0;
   wire [7:0] en_reg_pwm_15_8;
   wire [7:0] pwm_duty_cycle;
 
-  // Instantiate the PWM module
   pwm_peripheral pwm_peripheral_inst (
     .clk(clk),
     .rst_n(rst_n),
@@ -21,5 +34,7 @@ module tt_um_uwasic_onboarding_siratcha_boonsakdee(...)
     .pwm_duty_cycle(pwm_duty_cycle),
     .out({uio_out, uo_out})
   );
-  // Add uio_in and ui_in[7:3] to the list of unused signals:
+
   wire _unused = &{ena, ui_in[7:3], uio_in, 1'b0};
+
+endmodule
